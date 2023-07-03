f596fd9b28b69be4d55b513501826269a35b72d3
jhfgujr <99558450+jhfgujr@users.noreply.github.com>
2023-07-03 12:59:33 -0600

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
-
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
 function preload(){
   song1 = loadSound("music.mp3"); 
   song2 = loadSound("music.mp3"); 
@@ -28,6 +29,9 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist =" + scoreLeftWrist);
+
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX =" + leftWristX +"leftWristY =" + leftWristY);
@@ -40,6 +44,19 @@ function gotPoses(results){
 
 function draw(){
     image(video, 0, 0, 600, 500);
+    fill('#FF0000');
+    stroke('#FF0000');
+
+    if(scoreRightWrist > 0.2){
+        circle(leftWristX,leftWristY,20);
+        song2.stop();
+        }
+        
+        if(song1_status == false){
+        document.getElementById("song").innerHTML = "Reproduciendo: cancion de Harry Potter" + volume;
+        song1.play;
+        }
+    
 }
 
 function play(){
@@ -47,3 +64,4 @@ function play(){
     song.setVolume(1);
     song.rate(1);
 }
+
